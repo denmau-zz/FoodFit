1b000f950f00748d5bf5c5645d1fc5124f690125
Dennis Kamau <dmkamau475@gmail.com>
2021-01-30 22:12:46 +0300

    create registerUI

--- app/src/main/java/me/denmau/foodfit/LoginActivity.java
+++ app/src/main/java/me/denmau/foodfit/LoginActivity.java
@@ -3,25 +3,31 @@ package me.denmau.foodfit;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnRegister;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // instanciate views
+        // instantiate views
+        btnRegister = findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnRegister:
-                startActivity(new Intent(this, RegisterActivity.class));
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
                 break;
+
         }
     }
 
