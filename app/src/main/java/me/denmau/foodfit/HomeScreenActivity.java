0894390962f1280fbb9d11815a9a0340b02b0be9
Dennis Kamau <dmkamau475@gmail.com>
2021-02-03 07:02:22 +0300

    add bottom navigation

--- app/src/main/java/me/denmau/foodfit/HomeScreenActivity.java
+++ app/src/main/java/me/denmau/foodfit/HomeScreenActivity.java
@@ -2,22 +2,26 @@ package me.denmau.foodfit;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 
 import me.denmau.foodfit.reciperecycler.RecipeModel;
 import me.denmau.foodfit.ui.home.RecipesFragment;
+import me.denmau.foodfit.ui.random.RandomFragment;
+import me.denmau.foodfit.ui.search.SearchFragment;
 
-public class HomeScreenActivity extends AppCompatActivity {
-
-
+public class HomeScreenActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     /*
      * Created by Dennis Kamau
      * website: https://www.denmau.me
@@ -25,13 +29,17 @@ public class HomeScreenActivity extends AppCompatActivity {
 
     private ArrayList<RecipeModel> recipes = new ArrayList<>();
     private final String TAG = "HomeScreenActivity";
-    FirebaseUser currentUser;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        user = FirebaseAuth.getInstance().getCurrentUser();
 
+        //getting bottom navigation view and attaching the listener
+        BottomNavigationView bottomNav = findViewById(R.id.navigation);
+        bottomNav.setOnNavigationItemSelectedListener((BottomNavigationView.OnNavigationItemSelectedListener) this);
     }
 
     @Override
@@ -49,13 +57,43 @@ public class HomeScreenActivity extends AppCompatActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager
                 .beginTransaction();
-        Log.d(TAG, "recipesFragment is on display");
 
         // Add the Recipes Fragment.
-        fragmentTransaction.add(R.id.fragment_container, recipesFragment).addToBackStack(null).commit();
+        fragmentTransaction.add(R.id.fragment_container, recipesFragment).commit();
 
         Log.d(TAG, "recipesFragment attached successfully");
-
-        Toast.makeText(this, "recipesFragment is on display", Toast.LENGTH_SHORT).show();
     }
+
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = RecipesFragment.newInstance(recipes);
+                break;
+
+            case R.id.navigation_search:
+                fragment = new SearchFragment();
+                break;
+
+            case R.id.navigation_random:
+                fragment = new RandomFragment();
+                break;
+        }
+
+        return loadFragment(fragment);    }
 }
--- app/src/main/java/me/denmau/foodfit/MainActivity.java
+++ app/src/main/java/me/denmau/foodfit/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,27 +18,38 @@ public class MainActivity extends AppCompatActivity {
      * website: https://www.denmau.me
      */
 
-    // Firebase
-    private FirebaseAuth mAuth;
+    FirebaseUser user;
+    private final int DELAY = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAuth = FirebaseAuth.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        int DELAY = 3000;
+
+        if (user != null) {
+            Toast.makeText(this, "Hello, welcome back", Toast.LENGTH_LONG).show();
+            goToHomeScreen();
+        } else
+            goToLoginScreen();
+    }
+
+    private void goToHomeScreen() {
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            if (mAuth != null) {
-                // user already logged in, go to Home Screeen
-                Toast.makeText(this, "Hey " + mAuth.getCurrentUser().getDisplayName() + ", Welcome back", Toast.LENGTH_LONG).show();
-                startActivity(new Intent(MainActivity.this, HomeScreenActivity.class));
-            }
+
+            startActivity(new Intent(MainActivity.this, HomeScreenActivity.class));
+        }, DELAY);
+    }
+
+    private void goToLoginScreen() {
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
             startActivity(new Intent(MainActivity.this, AboutAppActivity.class));
         }, DELAY);
     }
+
 }
